83dfd9cc2ff854439d50891771fcae86e444c70f
Hetin Maheshwari <82659545+hetin@users.noreply.github.com>
2023-11-04 18:25:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Grid, Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import { blue, green } from '@mui/material/colors';
 
-export default function App() {
+const App = () => {
+  const [productData, setProductData] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    axios.get('https://fakestoreapi.com/products')
+      .then(response => {
+        setProductData(response.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        {productData.map((product, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card sx={{ backgroundColor: blue[50] }}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {product.title}
+                </Typography>
+                <Typography color="textSecondary">
+                  Price: ${product.price}
+                </Typography>
+                <Typography color="textSecondary">
+                  Category: {product.category}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => addToCart(product)}
+                  sx={{ backgroundColor: green[500], color: 'white' }}
+                >
+                  Add to Cart
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <div>
+        <Typography variant="h4" component="div" mt={4}>
+          Cart
+        </Typography>
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index}>
+              {item.title} - ${item.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
+  );
 }
+
+export default App;
